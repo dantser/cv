141018811ebe76f176aa9af03e190a2e410402c6
dantser <dantser@gmail.com>
2024-11-16 13:30:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Динамическое добавление навыков
+const skills = [
+    "Manual Testing",
+    "Functional Testing",
+    "Regression Testing",
+    "API Testing",
+    "Defect Tracking",
+    "SQL",
+    "Postman",
+    "Jira",
+    "Jenkins",
+    "Agile (Scrum, Kanban)"
+];
 
+const skillsContainer = document.getElementById('skills');
+
+skills.forEach(skill => {
+    const skillDiv = document.createElement('div');
+    skillDiv.textContent = skill;
+    skillsContainer.appendChild(skillDiv);
+});
